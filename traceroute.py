53bcf356cd790037d36a9b2f43eee62685527605
Ripunjay Singh <65473308+rsingh0616@users.noreply.github.com>
2023-04-16 11:24:52 -0400

    Update traceroute.py

--- traceroute.py
+++ traceroute.py
@@ -130,7 +130,7 @@ def get_route(hostname):
                 # Fill in end
                 try:  # try to fetch the hostname of the router that returned the packet - don't confuse with the hostname that you are tracing
                     # Fill in start
-                    routerHostname = gethostbyaddr(addr[0])
+                    routerHostname = gethostbyaddr(addr[0])[0]
                     # Fill in end
                 except herror:  # if the router host does not provide a hostname use "hostname not returnable"
                     # Fill in start
